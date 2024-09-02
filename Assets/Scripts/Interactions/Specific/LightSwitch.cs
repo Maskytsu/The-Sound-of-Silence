19c5411ff7387aeb2cb8feb89df53ac015cb09d0
Masky <biloxwolf@gmail.com>
2024-09-02 13:46:02 +0200

    rename folders, added lightSwitch, ws UI on top

---
+++ Assets/Scripts/Interactions/Specific/LightSwitch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : Interactable
+{
+    [SerializeField] private Transform _switchTransform;
+    [SerializeField] private List<GameObject> _lightSources = new List<GameObject>();
+    [SerializeField] private bool _turnedOn = false;
+
+    private void Awake()
+    {
+        SettLightsAndSwitch();
+    }
+
+    public override void Interact()
+    {
+        TurnOnOffLights();
+    }
+
+    private void TurnOnOffLights()
+    {
+        _turnedOn = !_turnedOn;
+        SettLightsAndSwitch();
+    }
+        private void SettLightsAndSwitch()
+    {
+        foreach (GameObject lightSource in _lightSources)
+        {
+            lightSource.SetActive(_turnedOn);
+        }
+
+        if (_turnedOn) _switchTransform.rotation = Quaternion.Euler(-15, 0, 0);
+        else _switchTransform.rotation = Quaternion.Euler(15, 0, 0);
+    }
+}

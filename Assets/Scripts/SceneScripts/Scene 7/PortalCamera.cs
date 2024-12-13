3abf9d29f1532b1a833db0763570cf1e4e596243
Masky <biloxwolf@gmail.com>
2024-12-13 09:21:49 +0100

    moved scripts, created OGs and small notes

--- Assets/Scripts/SceneScripts/Scene 7/PortalCamera.cs
+++ Assets/Scripts/SceneScripts/Scene 7/PortalCamera.cs
@@ -5,6 +5,8 @@ using RenderPipeline = UnityEngine.Rendering.RenderPipelineManager;
 
 public class PortalCamera : MonoBehaviour
 {
+    //those portal transforms must face the opposite side of the player view
+    //it's forward vector must be towards the not visable side
     [SerializeField] private Transform _visablePortalTransform;
     [SerializeField] private Renderer _visablePortalRenderer;
     [Space]
@@ -16,7 +18,6 @@ public class PortalCamera : MonoBehaviour
     [SerializeField] private Trigger _playerCloseTrigger;
 
     private RenderTexture _portalDisplayTexture;
-
     private bool _isPlayerClose = false;
 
     private void Awake()
@@ -30,6 +31,12 @@ public class PortalCamera : MonoBehaviour
 
         _playerCloseTrigger.OnObjectTriggerEnter += () => _isPlayerClose = true;
         _playerCloseTrigger.OnObjectTriggerExit += () => _isPlayerClose = false;
+    }
+
+    private void Update()
+    {
+        //MOŻNA SPAWNOWAĆ LATARKĘ NA PORTAL KAMERZE ŻEBY BYŁO TAKIE SAMO ŚWIATŁO ZA I PRZED PORTALEM
+        //POTWÓR MOŻE TEŻ TAM DAWAĆ JAKIEŚ ŚWIATŁO I TO MOŻE COŚ BUGOWAĆ - NA TRIGGERZE TEPAĆ POTWORA I WYŁĄCZAĆ MU ŚWIATŁO????
     }
 
     private void OnEnable()
@@ -47,7 +54,6 @@ public class PortalCamera : MonoBehaviour
         if (_visablePortalRenderer.isVisible || _isPlayerClose)
         {
             _portalCamera.targetTexture = _portalDisplayTexture;
-
             RenderCamera(_visablePortalTransform, _otherPortalTransform, SRC);
         }
     }
@@ -57,26 +63,26 @@ public class PortalCamera : MonoBehaviour
         _portalCamera.transform.position = _playerCamera.transform.position;
         _portalCamera.transform.rotation = _playerCamera.transform.rotation;
 
-        // Position the camera behind the other portal.
+        //position the camera behind the other portal
         Vector3 relativePos = visablePortal.InverseTransformPoint(_portalCamera.transform.position);
         relativePos = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativePos;
         _portalCamera.transform.position = otherPortal.TransformPoint(relativePos);
 
-        // Rotate the camera to look through the other portal.
+        //rotate the camera to look through the other portal
         Quaternion relativeRot = Quaternion.Inverse(visablePortal.rotation) * _portalCamera.transform.rotation;
         relativeRot = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativeRot;
         _portalCamera.transform.rotation = otherPortal.rotation * relativeRot;
 
-        // Set the camera's oblique view frustum.
-        Plane p = new Plane(-otherPortal.forward, otherPortal.position);
-        Vector4 clipPlaneWorldSpace = new Vector4(p.normal.x, p.normal.y, p.normal.z, p.distance);
+        //set the camera's oblique view frustum
+        Plane plane = new Plane(-otherPortal.forward, otherPortal.position);
+        Vector4 clipPlaneWorldSpace = new Vector4(plane.normal.x, plane.normal.y, plane.normal.z, plane.distance);
         Vector4 clipPlaneCameraSpace =
             Matrix4x4.Transpose(Matrix4x4.Inverse(_portalCamera.worldToCameraMatrix)) * clipPlaneWorldSpace;
 
         var newMatrix = _playerCamera.CalculateObliqueMatrix(clipPlaneCameraSpace);
         _portalCamera.projectionMatrix = newMatrix;
 
-        // Render the camera to its render target.
+        //render the camera to its render target
         UniversalRenderPipeline.RenderSingleCamera(SRC, _portalCamera);
     }
 }

94c5fdff34706ae0374439b1d4b5dc34a599c0d0
Masky <biloxwolf@gmail.com>
2025-01-09 12:24:52 +0100

    int doors, windows; 2 harry rooms; texture stair

--- Assets/Scripts/Testing/TestingScene.cs
+++ Assets/Scripts/Testing/TestingScene.cs
@@ -6,11 +6,6 @@ public class TestingScene : MonoBehaviour
 {
     [SerializeField, ReorderableList] private List<QuestScriptable> _quests;
 
-    private void Start()
-    {
-        AssignDoorEvents();
-    }
-
     [Button]
     private void StartFirstQuestFromList()
     {
@@ -21,15 +16,5 @@ public class TestingScene : MonoBehaviour
     private void EndFirstQuestFromList()
     {
         QuestManager.Instance.EndQuest(_quests[0]);
-    }
-
-    [HorizontalLine]
-    [SerializeField] private Door _door1;
-    [SerializeField] private Door _door2;
-
-    private void AssignDoorEvents()
-    {
-        _door1.InteractionHitbox.OnInteract += _door2.SwitchDoorAnimated;
-        _door2.InteractionHitbox.OnInteract += _door1.SwitchDoorAnimated;
     }
 }

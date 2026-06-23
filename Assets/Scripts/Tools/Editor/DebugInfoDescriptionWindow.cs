2d55ef0e1241639ebe88c8295457094ed48dd66d
Masky <makskulikowski0@gmail.com>
2026-06-23 14:57:12 +0200

    fixed gizmo settings

--- Assets/Scripts/Tools/Editor/DebugInfoDescriptionWindow.cs
+++ Assets/Scripts/Tools/Editor/DebugInfoDescriptionWindow.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 public class DebugInfoDescriptionWindow : EditorWindow
 {
     private string displayText =
-        "M - toggle monster interactions\n\n" +
-        "Shift - toggle sprint\n\n" +
-        "U - toggle time scale na 50\n\n" +
+        "Bindingi:\n" +
+        "M - toggle monster interactions\n" +
+        "Shift - toggle sprint\n" +
+        "U - toggle time scale na 50\n" +
         "N - toggle no clip\n\n" +
         "Ending setup - pod GameManagerem mamy obiekt GameplayManager i na nim komponent GameState, a tam są guziki\n" +
         "Ogólnie można to przejrzeć, bo tam są różne guziki na obiektach pod GameManagerem ;)\n\n" +
-        "Na scriptable'ach questów można je zaczynać i kończyć guzikiem";
+        "Na scriptable'ach questów można je zaczynać i kończyć guzikiem\n\n" +
+        "Gizmosy:\n" +
+        "- trigger: player (pink), monster (yellow)\n" +
+        "- interactable: glitch (pink), pickable (yellow), simple dialogue (green), lock (red), disabled (transparent)";
 
     [MenuItem("TSoS/Debug Info")]
     public static void ShowWindow()
--- Assets/Scripts/Tools/SceneViewGizmoSettings.cs
+++ Assets/Scripts/Tools/SceneViewGizmoSettings.cs
@@ -9,9 +9,9 @@ public class SceneViewGizmoSettings : Overlay
     public static bool DrawFullTriggers { get => EditorPrefs.GetBool("DrawFullTriggers", false); set => EditorPrefs.SetBool("DrawFullTriggers", value); }
     public static bool DrawPlayer { get => EditorPrefs.GetBool("DrawPlayer", true); set => EditorPrefs.SetBool("DrawPlayer", value); }
     public static bool DrawPlayerTargetPos { get => EditorPrefs.GetBool("DrawPlayerTargetPos", true); set => EditorPrefs.SetBool("DrawPlayerTargetPos", value); }
+    public static bool DrawInteractionHitbox { get => EditorPrefs.GetBool("DrawInteractionHitbox", false); set => EditorPrefs.SetBool("DrawInteractionHitbox", value); }
     public static bool DrawInteractableGizmo { get => EditorPrefs.GetBool("DrawInteractableGizmo", true); set => EditorPrefs.SetBool("DrawInteractableGizmo", value); }
     public static bool DivideInteractableGizmo { get => EditorPrefs.GetBool("DivideInteractableGizmo", true); set => EditorPrefs.SetBool("DivideInteractableGizmo", value); }
-    public static bool DrawInteractionHitbox { get => EditorPrefs.GetBool("DrawInteractionHitbox", false); set => EditorPrefs.SetBool("DrawInteractionHitbox", value); }
 
     public override VisualElement CreatePanelContent()
     {
@@ -19,35 +19,38 @@ public class SceneViewGizmoSettings : Overlay
         var toggleDrawFullTriggers = new Toggle("--Draw Full Triggers");
         var toggleDrawPlayer = new Toggle("Draw Player");
         var toggleDrawPlayerTargetPos = new Toggle("Draw Player Target Pos");
+        var toggleDrawInteractionHitbox = new Toggle("Draw Interaction Hitbox");
         var toggleDrawInteractableGizmo = new Toggle("Draw Interactable Gizmo");
         var toggleDivideInteractableGizmo = new Toggle("--Divide Interactable Gizmo");
-        var toggleDrawInteractionHitbox = new Toggle("Draw Interaction Hitbox");
 
         toggleDrawTriggers.value = DrawTriggers;
         toggleDrawFullTriggers.value = DrawFullTriggers;
         toggleDrawPlayer.value = DrawPlayer;
         toggleDrawPlayerTargetPos.value = DrawPlayerTargetPos;
+        toggleDrawInteractionHitbox.value = DrawInteractionHitbox;
         toggleDrawInteractableGizmo.value = DrawInteractableGizmo;
         toggleDivideInteractableGizmo.value = DivideInteractableGizmo;
-        toggleDrawInteractionHitbox.value = DrawInteractionHitbox;
 
         toggleDrawTriggers.RegisterValueChangedCallback(evt => DrawTriggers = evt.newValue);
         toggleDrawFullTriggers.RegisterValueChangedCallback(evt => DrawFullTriggers = evt.newValue);
         toggleDrawPlayer.RegisterValueChangedCallback(evt => DrawPlayer = evt.newValue);
         toggleDrawPlayerTargetPos.RegisterValueChangedCallback(evt => DrawPlayerTargetPos = evt.newValue);
+        toggleDrawInteractionHitbox.RegisterValueChangedCallback(evt => DrawInteractionHitbox = evt.newValue);
         toggleDrawInteractableGizmo.RegisterValueChangedCallback(evt => DrawInteractableGizmo = evt.newValue);
         toggleDivideInteractableGizmo.RegisterValueChangedCallback(evt => DivideInteractableGizmo = evt.newValue);
-        toggleDrawInteractionHitbox.RegisterValueChangedCallback(evt => DrawInteractionHitbox = evt.newValue);
+
+        toggleDrawFullTriggers.style.marginBottom = 15;
+        toggleDrawPlayerTargetPos.style.marginBottom = 15;
 
         var root = new VisualElement();
 
         root.Add(toggleDrawTriggers);
-        if (DrawTriggers) root.Add(toggleDrawFullTriggers);
+        root.Add(toggleDrawFullTriggers);
         root.Add(toggleDrawPlayer);
         root.Add(toggleDrawPlayerTargetPos);
-        root.Add(toggleDrawInteractableGizmo);
-        if (DrawInteractableGizmo) root.Add(toggleDivideInteractableGizmo);
         root.Add(toggleDrawInteractionHitbox);
+        root.Add(toggleDrawInteractableGizmo);
+        root.Add(toggleDivideInteractableGizmo);
 
         return root;
     }
